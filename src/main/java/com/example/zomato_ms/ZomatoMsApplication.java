cd6c3b45ee8ff648e00ca2e8bda2679d0a6408ed
surajdudhankar <surajdudhankar96@gmail.com>
2025-02-21 19:29:56 +0530

    zomato ms project

---
+++ src/main/java/com/example/zomato_ms/ZomatoMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.zomato_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zomato_ms/ZomatoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zomato_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
